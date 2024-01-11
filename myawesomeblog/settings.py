0b2fb0b2d58acc2bf9c511ead67857c467ec5916
Wenchenesse <wencheeh99j@gmail.com>
2024-01-11 19:55:29 +0300

    connect to postgre and add post models in blog

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,8 +77,12 @@ WSGI_APPLICATION = "myawesomeblog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myawesomeblogdb",
+        "USER":"postgres",
+        "PASSWORD": "wonwon",
+        "HOST":"localhost",
+        "PORT":"5432",
     }
 }
 
@@ -110,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/awesome_media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
