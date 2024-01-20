a0f2e36b4211faa4cf2efb9bd5d491c52ef1c404
Wenchenesse <wencheeh99j@gmail.com>
2024-01-20 05:23:44 +0300

    static files and specific posts

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,7 @@ class Post(models.Model):
     post_date = models.DateTimeField()
     post_text = models.TextField()
     post_image = models.ImageField(upload_to='post_images/')
+    
+    
+    def get_summary(self):
+        return self.text[:70]
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "myawesomeblog/static/"
+]
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
